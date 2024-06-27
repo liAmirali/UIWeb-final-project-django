8267a167be5590b814d96297a079199f2029975e
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-28 01:45:32 +0330

    Customize admin user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import AppUser
 
-# Register your models here.
+
+class AppUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+admin.site.register(AppUser)
