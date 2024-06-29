7c4c043daaa486518fd3161efb91662d88de1262
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-29 18:10:32 +0330

    Add initial object model

--- objects/models.py
+++ objects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class AppObject(models.Model):
+    name = models.CharField(max_length=100)
+    object_key = models.CharField(max_length=100)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        'auth.User', related_name='objects', on_delete=models.SET_NULL, null=True)
+    shared_with = models.ManyToManyField('auth.User', related_name='shared_objects')
+
+    def __str__(self):
+        return self.name
