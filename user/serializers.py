9b135039c45c10264e3060136c404279a25967cd
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-07-02 21:23:58 +0330

    Fix bug in signup API

--- user/serializers.py
+++ user/serializers.py
@@ -19,8 +19,8 @@ class SignUpSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User(
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
             username=validated_data['username'],
             email=validated_data['email']
         )
